024ff60d865b2570915f1b6b4a5482f122abce76
Alphonso Gillis <shadowtheguy912@gmail.com>
2026-01-08 21:13:10 -0600

    Added other CRUD operations to backend

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,67 @@ app.get("/mtgdecks/:id", async (req, res) => {
     return res.status(404).json({ error: error.message });
   }
 
-  res.status(200).json(data)
+  res.status(200).json(data);
+});
+
+app.post("/mtgdecks", async (req, res) => {
+  const { deck_title, deck_author, format, card_list, custom_cards } = req.body;
+
+  const newDeckList = {
+    deck_title,
+    deck_author,
+    format,
+    card_list,
+    custom_cards,
+  };
+
+  const { data, error } = await supabase
+    .from("MTG Deck Manager")
+    .insert(newDeckList)
+    .select();
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.status(201).json(data);
+});
+
+app.put("/mtgdecks/:id", async (req, res) => {
+  const { deck_title, deck_author, format, card_list, custom_cards } = req.body;
+
+  const updateDeckList = {
+    deck_title,
+    deck_author,
+    format,
+    card_list,
+    custom_cards,
+  };
+
+  const { data, error } = await supabase
+    .from("MTG Deck Manager")
+    .update(updateDeckList)
+    .eq("id", req.params.id)
+    .select();
+
+  if (error) {
+    return res.status(404).json({ error: error.message });
+  }
+
+  return res.status(201).json(data[0]);
+});
+
+app.delete("/mtgdecks/:id", async (req, res) => {
+  const { data, error } = await supabase
+    .from("MTG Deck Manager")
+    .delete()
+    .eq("id", req.params.id);
+
+  if (error) {
+    return res.status(404).json({ error: error.message });
+  }
+
+  return res.status(200).json({ message: "Deck deleted successfully" });
 });
 
 app.listen(PORT, () => {

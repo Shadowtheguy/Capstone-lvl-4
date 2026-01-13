9698fa034b0381fd1fd39994b76f00130f8de5e4
Alphonso Gillis <shadowtheguy912@gmail.com>
2026-01-12 20:35:06 -0600

    Microservice now fetches cards

--- microservice/index.js
+++ microservice/index.js
@@ -1,14 +1,35 @@
-import express from 'express';
-import 'dotenv/config';
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Microservice is running' });
+app.get("/", (req, res) => {
+  res.json({ message: "Microservice is running" });
 });
+
+app.get("/api/MTGcard/:name", async (req, res) => {
+  const name = req.params.name
+
+  const url = `https://api.scryfall.com/cards/named?fuzzy=${name}`
+
+  console.log(`Fetching information for cards related to ${name}`)
+
+  if(!name) {
+    return res.status(404).json({ error: "Name Required" })
+  }
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data)
+
+  res.status(201).json(data)
+})
 
 app.listen(PORT, () => {
   console.log(`Microservice running on port ${PORT}`);

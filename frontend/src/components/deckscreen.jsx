a2da03d037e1fad2da2dac355da2e63388481f7f
Alphonso Gillis <shadowtheguy912@gmail.com>
2026-01-28 19:45:16 -0600

    Made it so my deck screen fetches all created decks and displays them

--- frontend/src/components/deckscreen.jsx
+++ frontend/src/components/deckscreen.jsx
@@ -1,6 +1,10 @@
 import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+function translateToSearch(string) {
+  return string.replaceAll(" ", "+");
+}
+
 function DeckScreen() {
   //* Navigation
   const navigate = useNavigate();
@@ -23,7 +27,7 @@ function DeckScreen() {
         };
       });
   }
-  
+
   function getDeckList(deckData) {
     return {
       deckTitle: deckData.deck_title,
@@ -34,14 +38,48 @@ function DeckScreen() {
       deckPicture: deckData.deck_picture,
     };
   }
-  
-  function filterCreatedDecks(listData) {
-    const formattedList = listData.map(getDeckList);
-    setCreatedDecks(formattedList);
 
-    console.log(formattedList);
+  async function filterCreatedDecks(listData) {
+    const formattedList = await Promise.all(
+      listData.map(async (deckData) => {
+        const deck = getDeckList(deckData);
+
+        // Fetch image if deck has one
+        if (deck.deckPicture) {
+          try {
+            const cardInfo = await fetchDeckImage(deck.deckPicture);
+            deck.deckImageUrl = cardInfo.cardPicture;
+          } catch (err) {
+            console.error("Failed to fetch deck image", err);
+          }
+        }
+
+        return deck;
+      }),
+    );
+
+    setCreatedDecks(formattedList);
   }
-  
+
+  // Getting Card Images
+  function getCardinfo(cardData) {
+    return {
+      cardName: cardData.name,
+      manaCost: cardData.mana_cost,
+      cardType: cardData.type_line,
+      cardDescription: cardData.oracle_text,
+      cardPicture: cardData.image_uris.art_crop,
+    };
+  }
+
+  function fetchDeckImage(deckPicture) {
+    const fetchCard = translateToSearch(deckPicture);
+
+    return fetch("http://localhost:3001/api/MTGcardTest/" + fetchCard)
+      .then((response) => response.json())
+      .then(getCardinfo);
+  }
+
   // Set Up Page
   useEffect(() => {
     fetch("http://localhost:3000/mtgdecks")
@@ -95,6 +133,24 @@ function DeckScreen() {
         </div>
       </section>
       <hr />
+      <section className="container mt-4">
+        <div className="row">
+          {createdDecks.map((deck, index) => (
+            <div className="col-md-4 mb-4" key={index}>
+              <div className="card h-100">
+                <img
+                  src={deck.deckImageUrl}
+                  className="card-img-top"
+                  alt={deck.deckTitle}
+                />
+                <div className="card-body text-center">
+                  <h5 className="card-title text-white">{deck.deckTitle}</h5>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
